9a4ea5e4d8ec9007eb37f2ce17612245dbc6dd13
tanjoshua <jtanjoshua@gmail.com>
2021-12-22 16:12:02 -0500

    web listing service

--- web/services/listingService.ts
+++ web/services/listingService.ts
@@ -2,9 +2,27 @@ import axiosBase from "./base";
 
 const getListings = () => {
   const params = {};
-  return axiosBase("/listing", { params });
+  return axiosBase.get("/listing", { params });
 };
 
-const createListing = () => {};
+const createListing = (data) => {
+  return axiosBase.post("/listing", data);
+};
+
+const updateListing = (data) => {
+  return axiosBase.put("/listing", data);
+};
+
+const getListingById = (id) => {
+  return axiosBase.get(`/listing/${id}`);
+};
+
+const deleteListingById = (id) => {
+  return axiosBase.delete(`/listing/${id}`);
+};
+
+const getLevels = () => {
+  return axiosBase.get(`/listing/levels`);
+};
 
 export default {};
