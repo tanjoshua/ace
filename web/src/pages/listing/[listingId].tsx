01408c14b73484d5b29be3a0b9759c0ed5bb6667
tanjoshua <jtanjoshua@gmail.com>
2022-01-05 16:08:22 -0500

    listing details + title bugfix

---
+++ web/src/pages/listing/[listingId].tsx
@@ -0,0 +1,67 @@
+import {
+  Flex,
+  Heading,
+  Stack,
+  Text,
+  Tag,
+  LinkBox,
+  LinkOverlay,
+} from "@chakra-ui/react";
+import React from "react";
+import listingService from "../../../services/listingService";
+import useFetch from "../../../utils/useFetch";
+import Navbar from "../../components/shared/Navbar";
+
+interface Props {
+  listingId: string;
+}
+
+const ListingPage = ({ listingId }: Props) => {
+  const [isLoading, response, error] = useFetch(() =>
+    listingService.getListingById(listingId)
+  );
+  const listing = response?.data;
+
+  return (
+    <>
+      <Navbar />
+      {listing && (
+        <Stack padding={2}>
+          <Heading>{listing.title}</Heading>
+          <Heading size="md">Subject(s)</Heading>
+          <Flex>
+            {listing.subject.map((subject) => (
+              <Tag key={subject} mr={2}>
+                {subject}
+              </Tag>
+            ))}
+          </Flex>
+          <Heading size="md">Level(s)</Heading>
+          <Flex>
+            {listing.level.map((subject) => (
+              <Tag key={subject} mr={2}>
+                {subject}
+              </Tag>
+            ))}
+          </Flex>
+          <Heading size="md">Description</Heading>
+          <Text>{listing.description}</Text>
+          <Heading size="md">Pricing</Heading>
+          <Text>{listing.pricing}</Text>
+          <Heading size="md">Contact Information</Heading>
+          <Text>{listing.contactInfo}</Text>
+          <Heading size="md">About The Tutor</Heading>
+          <LinkBox p={5} shadow="md" borderWidth="1px">
+            <LinkOverlay href={`/user/${listing.tutor.id}`}>
+              <Heading size="sm">{listing.tutor.name}</Heading>
+            </LinkOverlay>
+          </LinkBox>
+        </Stack>
+      )}
+    </>
+  );
+};
+
+ListingPage.getInitialProps = ({ query }) => ({ listingId: query.listingId });
+
+export default ListingPage;
