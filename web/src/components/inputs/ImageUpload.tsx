abaaba0b46bca195cf0fcf03a8a0bf296449afcc
tanjoshua <jtanjoshua@gmail.com>
2022-01-12 17:17:24 -0500

    fix image uploading

--- web/src/components/inputs/ImageUpload.tsx
+++ web/src/components/inputs/ImageUpload.tsx
@@ -4,20 +4,26 @@ import React, { useRef, useState } from "react";
 
 interface Props {
   name: string;
+  defaultImage?: string;
 }
 
-const ImageUpload = ({ ...props }: Props) => {
+const ImageUpload = ({ defaultImage, ...props }: Props) => {
   const [field, { error }, helpers] = useField(props);
   const { setValue } = helpers;
 
   const imageInputRef = useRef(null);
   const [profile, setProfile] = useState(null);
 
+  // default image
+  const dImage =
+    defaultImage ||
+    "https://t4.ftcdn.net/jpg/02/15/84/43/240_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg";
+
   const handleImageUpload = (event) => {
     let reader = new FileReader();
     let file = event.target.files[0];
     reader.onloadend = () => {
-      setValue(reader.result);
+      setValue(file);
       setProfile(reader.result);
     };
     reader.readAsDataURL(file);
@@ -35,10 +41,7 @@ const ImageUpload = ({ ...props }: Props) => {
       <Image
         borderRadius="full"
         boxSize="150px"
-        src={
-          profile ||
-          "https://t4.ftcdn.net/jpg/02/15/84/43/240_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg"
-        }
+        src={profile || dImage}
         alt="Profile Picture"
         onClick={() => imageInputRef.current.click()}
       />
